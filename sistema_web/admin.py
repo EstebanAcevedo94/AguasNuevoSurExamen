5fabb7fa537bb49bddd73b4f656e205821d35eb3
Esteban Acevedo <esteban.acevedo.1994@gmail.com>
2023-12-12 08:42:56 -0300

    Initial commit

--- sistema_web/admin.py
+++ sistema_web/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+
+class User_Admin(admin.ModelAdmin):
+    list_display = ('rut', 'nombre', 'apellido', 'sector', 'estado', 'tipo_usuario')
+    search_fields = ['rut']
+    list_filter = ('rut', 'nombre', 'apellido', 'sector', 'estado', 'tipo_usuario')
+
+
+class Lectura_Admin(admin.ModelAdmin):
+    list_display = ('codigo_lectura', 'usuario_realiza_lectura', 'usuario_toma_lectura')
+    search_fields = ['codigo_lectura']
+    list_filter = ('codigo_lectura', 'usuario_realiza_lectura', 'usuario_toma_lectura')
+
+
+class Pago_Admin(admin.ModelAdmin):
+    list_display = ('codigo_pago', 'usuario_registra_pago', 'usuario_recibe_pago', 'pago')
+    search_fields = ['codigo_pago', 'usuario_registra_pago']
+    list_filter = ('codigo_pago', 'usuario_registra_pago', 'usuario_recibe_pago', 'pago')
